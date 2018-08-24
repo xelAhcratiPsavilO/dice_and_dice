0a7ca311fa8426d428d51ae71ca3aba755c71f24
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-08-24 12:33:46 +0100

    Added ability to enquire_topci, Green phase

---
+++ lib/dnd.rb
@@ -0,0 +1,7 @@
+class Dnd
+
+  def enquire_topic
+    p "What question should I answer for you?"
+  end
+
+end
--- spec/dnd_spec.rb
+++ spec/dnd_spec.rb
@@ -1,10 +1,10 @@
 require 'dnd'
 
-describe DND do
+describe Dnd do
 
   describe '#enquire_topic' do
     it 'aks the user about a topic' do
-      expect(subject.enquire_topic).to eq("What question should I answer for you?")
+      expect(subject.enquire_topic).to eq('What question should I answer for you?')
     end
   end
 
