076efdd80521d0a4d84c2b6a42f895df92201155
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-08-24 12:24:46 +0100

    Added ability to enquire_topic, Red phase

---
+++ spec/dnd_spec.rb
@@ -0,0 +1,11 @@
+require 'dnd'
+
+describe DND do
+
+  describe '#enquire_topic' do
+    it 'aks the user about a topic' do
+      expect(subject.enquire_topic).to eq("What question should I answer for you?")
+    end
+  end
+
+end
