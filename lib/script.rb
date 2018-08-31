3928dc58d5b233838330807175e7bdd8b52d072b
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-08-31 14:55:21 +0100

    Added script, code to be refactored

---
+++ lib/script.rb
@@ -0,0 +1,19 @@
+require_relative 'dnd'
+
+my_dnd = Dnd.new
+
+my_dnd.enquire_topic
+my_dnd.get_topic
+my_dnd.enquire_choice
+my_dnd.get_choice1
+my_dnd.get_choice2
+my_dnd.show_input('choice2')
+my_dnd.overwrite_input('choice2')
+my_dnd.enquire_option
+my_dnd.get_option11
+my_dnd.get_option12
+my_dnd.collect_options_for_choice1
+my_dnd.get_option21
+my_dnd.get_option22
+my_dnd.collect_options_for_choice2
+my_dnd.answer
